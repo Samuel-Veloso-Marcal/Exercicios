a456151d30e0e6dda1bfe0e8f03b9142858c1a08
Samuel Veloso Marçal <samuel.veloso.marcal@gmail.com>
2020-02-07 17:39:25 -0200

    110 - Entidade clientes finalizada, avanço no programa central

---
+++ src/Enteties/Client.java
@@ -0,0 +1,58 @@
+package Enteties;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Client {
+	private Scanner sc = new Scanner(System.in);
+	private String name;
+	private String email;
+	private Date birth;
+	private List<Order> orders = new ArrayList<>();
+	
+	public Client(String name, String email, Date birth) {
+		this.name = name;
+		this.email = email;
+		this.birth = birth;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void addOrders(int n) {
+		for (int i=1; i<=n; i++) {
+			System.out.printf("Enter #%d item data:%nProduct name: ", i);
+			String name = sc.nextLine();
+			System.out.printf("Product price: ");
+			double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			Order x = new Order(name, price, quantity);
+			this.orders.add(x);
+		}
+	}
+
+	
+	
+	
+}
+
--- src/Program/Executable.java
+++ src/Program/Executable.java
@@ -1,5 +1,22 @@
 package Program;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import Enteties.Client;
+
 public class Executable {
-	
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdft = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.printf("Enter client data:%nName: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birth = sdft.parse(sc.nextLine());
+		Client client = new Client(name, email, birth);
+	}
 }
