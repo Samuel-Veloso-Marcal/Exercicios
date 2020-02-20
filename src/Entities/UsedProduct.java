daa0cfcefbb469e0206509d90bc55df9a9c1eadd
Samuel Veloso Marçal <samuel.veloso.marcal@gmail.com>
2020-02-19 23:40:19 -0300

    127 - UsedProduct finalizado

--- src/Entities/UsedProduct.java
+++ src/Entities/UsedProduct.java
@@ -1,5 +1,34 @@
 package Entities;
 
-public class UsedProduct extends Product{
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class UsedProduct extends Product{
+	private Date manufactureDate;
+	
+	public UsedProduct() {
+		super();
+	}
+	public UsedProduct(String name, double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+	public String getManufactureDateString() {
+		SimpleDateFormat sdft = new SimpleDateFormat("dd/MM/yyyy");
+		return sdft.format(manufactureDate);
+	}
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	@Override
+	public final String priceTag() {
+		return "(used) $ " + getPrice(); 
+	}
+	@Override
+	public final String toString() {
+		return super.toString() + "(Manafacture date : " + getManufactureDateString() + ")";
+	}
 }
--- src/Program/Executable.java
+++ src/Program/Executable.java
@@ -1,10 +1,33 @@
 package Program;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Entities.Product;
 import Entities.ImportedProduct;
+import Entities.UsedProduct;
 
 public class Executable {
 	public static void main(String[] args) {
-		ImportedProduct x = new ImportedProduct("josé", 400, 4);
-		System.out.println(x);
-}
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		List<Product> products = new ArrayList<Product>();
+		for (int i=1; i>=n; i++) {
+			System.out.printf("Product #%d data:%n Common, used or imported (c/u/i)? ", i);
+			char decision = sc.nextLine().charAt(0);
+			if (decision != 'i' && decision != 'c' && decision != 'u') {
+				System.out.print("Repeat (c/u/i): ");
+				decision = sc.nextLine().charAt(0);
+			}
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			if (decision == 'c') {
+				products.add(e)
+			}
+		}
+	}
 }
