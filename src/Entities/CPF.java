8410cc72ca6d03b80f9e914b83b39bf1a5f89e35
Samuel Veloso Marçal <samuel.veloso.marcal@gmail.com>
2020-02-26 22:07:10 -0300

    130 - Entidade CPF

--- src/Entities/CPF.java
+++ src/Entities/CPF.java
@@ -1,5 +1,28 @@
 package Entities;
 
 public class CPF extends CP{
-
+	private double saude;
+	public double getSaude() {
+		return saude;
+	}
+	public void setSaude(double saude) {
+		this.saude = saude;
+	}
+	public CPF(String name, double renda, double saude) {
+		super(name, renda);
+		this.saude = saude;
+	}
+	public CPF() {
+		super();
+	}
+	@Override
+	public final Double imposto() {
+		double abate = 0.5 * saude;
+		if (getRenda()<20000) {
+			return getRenda() * 0.15 - abate;
+		}
+		else {
+			return getRenda() * 0.25 - abate;
+		}
+	}
 }
